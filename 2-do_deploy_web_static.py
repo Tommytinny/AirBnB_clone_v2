c629d83ba2c6cefdcc44d5567ee91f44e4453870
Tommytinny <tommyteenboi@gmail.com>
2024-06-05 00:51:44 +0100

    added updated deployment files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ that distributes an archive to your web servers
 """
 from fabric.api import *
 from os.path import exists
-env.hosts = ['18.233.64.33', '18.204.10.26']
+env.hosts = ['100.26.153.245', '100.26.162.10']
 
 
 def do_deploy(archive_path):
@@ -17,13 +17,14 @@ def do_deploy(archive_path):
         file_no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run("mkdir -p {}{}/".format(path, file_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, file_no_ext))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, file_no_ext))
-        run("rm -rf {}{}/web_static".format(path, file_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, file_no_ext))
+        sudo("mkdir -p {}{}/".format(path, file_no_ext))
+        sudo("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, file_no_ext))
+        sudo("rm /tmp/{}".format(file_name))
+        sudo("mv {0}{1}/web_static/* {0}{1}/".format(path, file_no_ext))
+        sudo("rm -rf {}{}/web_static".format(path, file_no_ext))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {}{}/ /data/web_static/current".format(path, file_no_ext))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
