3a516dd7076164240a16a7281e0ef99ff3ed6d98
Tommytinny <tommyteenboi@gmail.com>
2024-06-05 01:35:28 +0100

    updated deployment fabfiles

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.api import *
 from datetime import datetime
 from os.path import isdir
 from os.path import exists
-env.hosts = ['18.233.64.33', '18.204.10.26']
+env.hosts = ['100.26.153.245', '100.26.162.10']
 
 
 def do_pack():
@@ -33,13 +33,13 @@ def do_deploy(archive_path):
         file_no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run("mkdir -p {}{}/".format(path, file_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, file_no_ext))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, file_no_ext))
-        run("rm -rf {}{}/web_static".format(path, file_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, file_no_ext))
+        sudo("mkdir -p {}{}/".format(path, file_no_ext))
+        sudo("tar -xzf /tmp/{} -C {}{}/".format(file_name, path, file_no_ext))
+        sudo("rm /tmp/{}".format(file_name))
+        sudo("mv {0}{1}/web_static/* {0}{1}/".format(path, file_no_ext))
+        sudo("rm -rf {}{}/web_static".format(path, file_no_ext))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {}{}/ /data/web_static/current".format(path, file_no_ext))
         return True
     except Exception as e:
         return False
